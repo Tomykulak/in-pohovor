51fa1e7add69a3d7531aa60f1aefbbf8b36b074e
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-11-29 21:53:41 +0100

    SoftDeleteCourt

--- TennisApp/src/main/java/cz/inqool/tennisapp/domain/court/CourtController.java
+++ TennisApp/src/main/java/cz/inqool/tennisapp/domain/court/CourtController.java
@@ -1,5 +1,11 @@
 package cz.inqool.tennisapp.domain.court;
 
+import cz.inqool.tennisapp.utils.exceptions.NotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api/court")
@@ -10,4 +16,14 @@ public class CourtController {
         this.courtService = courtService;
     }
 
+
+    @DeleteMapping(value = "/{id}", produces = "application/json")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
+    public void deleteCourt(@PathVariable int id) {
+        Court court = courtService
+                .getCourtById(id)
+                .orElseThrow(NotFoundException::new);
+        courtService.deleteCourt(court);
+    }
 }
--- TennisApp/src/main/java/cz/inqool/tennisapp/domain/court/CourtService.java
+++ TennisApp/src/main/java/cz/inqool/tennisapp/domain/court/CourtService.java
@@ -2,12 +2,18 @@ package cz.inqool.tennisapp.domain.court;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CourtService {
     private final CourtRepository courtRepository;
 
     public CourtService(CourtRepository courtRepository) {
         this.courtRepository = courtRepository;
+    }
+
+    public Optional<Court> getCourtById(int id) {
+        return courtRepository.findById((long) id);
     }
 
     public Court createCourt(Court court) {
@@ -20,6 +26,7 @@ public class CourtService {
 
     // only soft delete, so update isDeleted to true
     public void deleteCourt(Court court) {
+        court.setDeleted(true);
         courtRepository.save(court);
     }
 }
