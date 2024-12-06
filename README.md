dd1c1337615dc4f8299b7c39725d5485f888c45e
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-12-06 18:04:04 +0100

    Added swagger, needs more annotations in controlers

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 
 # Main url: http://localhost:8080/api/
 # H2 in-memory DB: http://localhost:8080/h2-console/
-
+- JDBC URL: jdbc:h2:mem:tennis-db
+- User Name: sa
+- Password: 
+# Swagger docs: http://localhost:8080/docs
 
 # All tennis clubs
 - http://localhost:8080/api/tennis-club
--- TennisApp/src/main/java/cz/inqool/tennisapp/domain/court/CourtController.java
+++ TennisApp/src/main/java/cz/inqool/tennisapp/domain/court/CourtController.java
@@ -6,6 +6,11 @@ import cz.inqool.tennisapp.domain.reservation.ReservationService;
 import cz.inqool.tennisapp.utils.exceptions.NotFoundException;
 import cz.inqool.tennisapp.utils.response.ArrayResponse;
 import cz.inqool.tennisapp.utils.response.ObjectResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/court")
+@Tag(name = "Courts", description = "Courts endpoint")
 public class CourtController {
     private final CourtService courtService;
     private final ReservationService reservationService;
@@ -24,6 +30,10 @@ public class CourtController {
     }
 
     @GetMapping(value = "", produces = "application/json")
+    @Operation(summary = "Returns all courts", description = "Returns all courts")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "returns all courts.")
+    })
     public ArrayResponse<CourtResponse> getAllCourts(){
         List<Court> courts = courtService.getAllCourts();
         return ArrayResponse.of(
@@ -33,6 +43,10 @@ public class CourtController {
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
+    @Operation(summary = "Return court by id.", description = "Return court by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return court by id.")
+    })
     public ObjectResponse<CourtResponse> getCourtById(@PathVariable int id) {
         Court court = courtService.getCourtById(id);
         if (court == null) {
