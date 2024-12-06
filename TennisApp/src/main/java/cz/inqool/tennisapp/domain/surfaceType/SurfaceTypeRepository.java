756196c1a13a766c4c212d508dd28e010ca9af48
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-12-06 19:21:41 +0100

    surfaceRepo

--- TennisApp/src/main/java/cz/inqool/tennisapp/domain/surfaceType/SurfaceTypeRepository.java
+++ TennisApp/src/main/java/cz/inqool/tennisapp/domain/surfaceType/SurfaceTypeRepository.java
@@ -2,5 +2,8 @@ package cz.inqool.tennisapp.domain.surfaceType;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SurfaceTypeRepository extends CrudRepository<SurfaceType, Long> {
+    List<SurfaceType> findByDeletedFalse();
 }
