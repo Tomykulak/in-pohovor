471a8f1d13504535463ec26eb666951aa0095203
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-12-10 00:10:42 +0100

    Customer repository test

--- TennisApp/src/test/java/cz/inqool/tennisapp/domain/customer/CustomerRepositoryTest.java
+++ TennisApp/src/test/java/cz/inqool/tennisapp/domain/customer/CustomerRepositoryTest.java
@@ -1,11 +1,39 @@
 package cz.inqool.tennisapp.domain.customer;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
 public class CustomerRepositoryTest {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @Test
-    void getAllCustomers(){}
+    void findByPhoneNumberExists(){
+        Customer customer = new Customer("tester", "123");
+        customerRepository.save(customer);
+
+        Optional<Customer> customerTest = customerRepository.findByPhoneNumber("123");
+
+        assertTrue(customerTest.isPresent());
+        Assertions.assertEquals("123", customerTest.get().getPhoneNumber());
+    }
+
+    @Test
+    void findByPhoneNumberNotExists(){
+        Customer customer = new Customer("tester", "123");
+        customerRepository.save(customer);
+
+        Optional<Customer> customerTest = customerRepository.findByPhoneNumber("456");
+
+        assertFalse(customerTest.isPresent());
+    }
 }
