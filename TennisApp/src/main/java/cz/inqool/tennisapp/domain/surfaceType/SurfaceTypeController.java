55f55a628433430c8bc88e447a3c28b6d5137057
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-12-09 21:12:16 +0100

    Surface Controller C.R.U.D

--- TennisApp/src/main/java/cz/inqool/tennisapp/domain/surfaceType/SurfaceTypeController.java
+++ TennisApp/src/main/java/cz/inqool/tennisapp/domain/surfaceType/SurfaceTypeController.java
@@ -1,14 +1,14 @@
 package cz.inqool.tennisapp.domain.surfaceType;
 
 import cz.inqool.tennisapp.utils.response.ArrayResponse;
+import cz.inqool.tennisapp.utils.response.ObjectResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +28,20 @@ public class SurfaceTypeController {
         List<SurfaceType> surfaceTypes = surfaceTypeService.getAllSurfaceTypes();
         return ArrayResponse.of(surfaceTypes, SurfaceTypeResponse::new);
     }
+
+    @PostMapping("")
+    public ObjectResponse<SurfaceTypeResponse> createSurfaceType(@Valid @RequestBody SurfaceType surfaceType){
+        return ObjectResponse.of(surfaceTypeService.createSurfaceType(surfaceType), SurfaceTypeResponse::new);
+    }
+
+    @PutMapping("/{id}")
+    public ObjectResponse<SurfaceTypeResponse> updateSurfaceType(@PathVariable int id, @Valid @RequestBody SurfaceType surfaceType){
+        return ObjectResponse.of(surfaceTypeService.updateSurfaceType(id, surfaceType), SurfaceTypeResponse::new);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSurfaceType(@PathVariable int id){
+        surfaceTypeService.deleteSurfaceType(id);
+    }
+
 }
