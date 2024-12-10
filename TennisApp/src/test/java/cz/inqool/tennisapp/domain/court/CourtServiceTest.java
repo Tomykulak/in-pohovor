f811dc265248a909d2b60e6e4da8380fbddffdc6
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-12-10 20:40:08 +0100

    CourtServiceTests

--- TennisApp/src/test/java/cz/inqool/tennisapp/domain/court/CourtServiceTest.java
+++ TennisApp/src/test/java/cz/inqool/tennisapp/domain/court/CourtServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
